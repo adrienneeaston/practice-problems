e5b2adfbb816e90f54984c2ce7dfc13c52950ba8
Adrienne <adrienneeaston@gmail.com>
2020-06-18 23:18:33 -0700

    destructuring with rest param

--- app.js
+++ app.js
@@ -74,3 +74,15 @@ let a = 8, b = 6;
 console.log(a); // 6
 console.log(b); // 8
 
+// destructuring using rest parameter
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  const [a, b, ...arr] = list; 
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+console.log(arr);
+
