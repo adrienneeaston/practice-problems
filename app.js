fcb8600f32520b589b809ce59ae6f0ca9af3b929
Adrienne <adrienneeaston@gmail.com>
2020-06-22 17:00:39 -0700

    regex userame passing

--- app.js
+++ app.js
@@ -192,5 +192,13 @@ makeServerRequest.catch(error => {
 // regix - username
 
 let username = "JackOfAllTrades";
-let userCheck = /^\D\w{1,}/; // Change this line
+let userCheck = /(?:^[A-Z]{2,}|^[A-Z][0-9]{2})[0-9]*$/i; // Change this line
 let result = userCheck.test(username);
+
+// 1) Usernames can only use alpha-numeric characters.
+
+// 2) The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+// 3) Username letters can be lowercase and uppercase.
+
+// 4) Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
