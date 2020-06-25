f8cbd349d6410852b02480f8c625c454f46c98a0
Adrienne <adrienneeaston@gmail.com>
2020-06-25 00:01:22 -0700

    modified array in object

--- app.js
+++ app.js
@@ -302,3 +302,33 @@ function getArrayOfUsers(obj) {
 }
 
 console.log(getArrayOfUsers(users));
+
+// Modify an array stored in an object
+
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  let friendArr = userObj.data.friends;
+  friendArr.push(friend);
+  return friendArr;
+}
+
+console.log(addFriend(user, 'Pete'));
