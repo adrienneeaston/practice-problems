3a90e797cd1d61e8221dbee796ed78ce6491b978
Adrienne <adrienneeaston@gmail.com>
2020-06-28 00:36:45 -0700

    add method after inheritance

--- app.js
+++ app.js
@@ -557,3 +557,35 @@ function Dog() { }
 Dog.prototype = Object.create(Animal.prototype);
 
 let beagle = new Dog();
+
+// Reset an Inherited Constructor Property
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// below duck and beagle will now say they are from Bird and Dog constructors rather than Animal
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
+
+// Add methods after inheritance
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function() {
+console.log("Woof!");
+}
+
+let beagle = new Dog();
