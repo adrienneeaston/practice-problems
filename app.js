71d55db3243165979a5f37c717741f19257332d3
Adrienne <adrienneeaston@gmail.com>
2020-06-27 23:36:23 -0700

    iterate over own and prototype properties

--- app.js
+++ app.js
@@ -489,3 +489,24 @@ for (let property in canary) {
     ownProps.push(property);
   }
 }
+
+// Iterate over all properties - own and prototype
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+  if(beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
