50950d9be5b77c8d40ac06ac0592695de5a8c28e
Adrienne <adrienneeaston@gmail.com>
2020-06-28 00:41:11 -0700

    override inherited methods

--- app.js
+++ app.js
@@ -589,3 +589,17 @@ console.log("Woof!");
 }
 
 let beagle = new Dog();
+
+// Override Inherited methods
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function() {return "Alas, this is a flightless bird."}
+
+let penguin = new Penguin();
+console.log(penguin.fly());
