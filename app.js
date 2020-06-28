49f0487c1f2e2f2f7e22e7f0a5465b05a13a670a
Adrienne <adrienneeaston@gmail.com>
2020-06-28 01:05:39 -0700

    use IIFE to create modeule

--- app.js
+++ app.js
@@ -639,3 +639,20 @@ function Bird() {
 (function () {
   console.log("A cozy nest is ready");
 })();
+
+// Use IIFE to create module
+
+let funModule = (function () {
+  return{
+    isCuteMixin: function(obj) {
+      obj.isCute = function() {
+        return true;
+      };
+    },
+    singMixin: function(obj) {
+      obj.sing = function() {
+        console.log("Singing to an awesome tune");
+      };
+    }
+  }
+})();
