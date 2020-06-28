c343ce148988471e96365dcc16f27d2f5fdbde74
Adrienne <adrienneeaston@gmail.com>
2020-06-28 00:14:09 -0700

    child/parent inheritance

--- app.js
+++ app.js
@@ -540,3 +540,20 @@ Dog.prototype = {
     console.log("My name is " + this.name);
   }
 };
+
+// Set the Child's Prototype to an Instance of the Parent
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
