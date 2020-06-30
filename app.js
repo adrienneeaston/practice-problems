b2e9cb6c2e84ae6694f8f777a1ba2f7bbd29ab12
Adrienne <adrienneeaston@gmail.com>
2020-06-30 16:13:05 -0700

    concat instead of push

--- app.js
+++ app.js
@@ -988,3 +988,12 @@ function nonMutatingConcat(original, attach) {
 var first = [1, 2, 3];
 var second = [4, 5];
 nonMutatingConcat(first, second);
+
+// adds element to end of an array with concat instead of push
+
+function nonMutatingPush(original, newItem) {
+  return original.concat(newItem);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
