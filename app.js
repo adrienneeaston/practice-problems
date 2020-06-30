e5e58c0ce1c0cf65cd6e6f6aa0abb905aaf6b01d
Adrienne <adrienneeaston@gmail.com>
2020-06-30 16:09:55 -0700

    array concatonation

--- app.js
+++ app.js
@@ -979,3 +979,12 @@ function nonMutatingSplice(cities) {
 }
 var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 nonMutatingSplice(inputCities);
+
+// array concatonation
+
+function nonMutatingConcat(original, attach) {
+  return original.concat(attach);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
