b6f7466117babf7c8c6e07feec3e807d94e7dd11
Adrienne <adrienneeaston@gmail.com>
2020-07-01 18:05:59 -0700

    diff two arrays

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -17,3 +17,24 @@ function sumAll(arr) {
 
 console.log(sumAll([4, 1]));
 
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  arr1.forEach(element => {
+    if(!arr2.includes(element)) {
+      newArr.push(element);
+    }
+  })
+
+  arr2.forEach(element => {
+    if(!arr1.includes(element)) {
+      newArr.push(element);
+    }
+  })
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
