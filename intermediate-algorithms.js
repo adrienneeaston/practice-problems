3238a1e7a6aedb2a97f336842333ea48740d113a
Adrienne <adrienneeaston@gmail.com>
2020-07-01 19:31:09 -0700

    arguments object

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -38,3 +38,21 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// arguments object - You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+function destroyer(arr) {
+  let args = Array.from(arguments).slice(1);
+
+  for (let i = arr.length - 1; i > -1; i--) {
+    for (let j = 0; j <= args.length; j++) {
+      if(arr[i] === args[j]){
+        arr.splice(i, 1);
+      }
+    }
+  }
+
+  return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
