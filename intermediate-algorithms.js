97efbb1f3bdf420276fc6e6c64e1c912b6af4c67
Adrienne <adrienneeaston@gmail.com>
2020-07-01 17:09:43 -0700

    sum all numbers in a range

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -1,2 +1,19 @@
 // INTERMEDIATE ALGORITHMS
 
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+function sumAll(arr) {
+  let lesserValue = Math.min(...arr);
+  let greaterValue = Math.max(...arr);
+  let total = 0;
+
+  while(lesserValue < greaterValue + 1) {
+    total = total + lesserValue;
+    lesserValue++;
+  }
+
+  return total;
+}
+
+console.log(sumAll([4, 1]));
+
