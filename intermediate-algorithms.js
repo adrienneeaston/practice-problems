9f555e64d6c02084c9111e47c76e6c8c2e9bb4a8
Adrienne <adrienneeaston@gmail.com>
2020-07-03 17:30:19 -0700

    for...in

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -90,3 +90,38 @@ function whatIsInAName(collection, source) {
 
 console.log(whatIsInAName([{ first: "Tybalt", last: "Capulet", color: "green" }], { first: "Tybalt", last: "Capulet" }));
 
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+function whatIsInAName(collection, source) {
+  var arr = [];
+
+  // Only change code below this line
+
+  collection.forEach(element => {
+    let match = true;
+    for (const key in source){
+      if (source[key] !== element[key]) {
+        match = false;
+      }
+    }
+    if(match) {
+      arr.push(element);
+    }
+  });
+  return arr;
+
+  // return collection.filter(element => {
+  //   let match = true;
+  //   for(const key in source){
+  //     if(source[key] !== element[key]) {
+  //       match = false;
+  //     }
+  //   }
+
+  //   return match;
+  // });
+  // Only change code above this line
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
