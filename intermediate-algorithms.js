e9a78391dd0b67196b66c80717c1b0894b586a36
Adrienne <adrienneeaston@gmail.com>
2020-07-12 17:16:05 -0700

    unknown number of arguments

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -259,4 +259,21 @@ function fearNotLetter(str) {
 
 fearNotLetter("abce");
 
+// unknown number of arguments - Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+function uniteUnique(arr) {
+  let args = [...arguments];
+  let newArr = [];
+  for(let i = 0; i < args.length; i++) {
+    for(let j = 0; j < args[i].length; j++) {
+      if(newArr.includes(args[i][j]) === false) {
+        newArr.push(args[i][j]);
+      }
+    }
+  }
+  return newArr;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
 
