ea621bf7ba3e4bd04627a4323029dd3a599cc582
Adrienne <adrienneeaston@gmail.com>
2020-07-17 17:28:01 -0700

    is element in object truthy

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -454,3 +454,18 @@ function binaryAgent(str) {
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+function truthCheck(collection, pre) {
+  let flag = true;
+  for(let i = 0; i < collection.length; i++) {
+    if(!collection[i][pre]) {
+      flag = false;
+    }
+  }
+
+  return flag;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
