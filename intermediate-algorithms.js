07dd265e425e565ecc6790d698eefda1ec95f4d0
Adrienne <adrienneeaston@gmail.com>
2020-07-18 23:58:01 -0700

    add numbers, ot finished

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -469,3 +469,11 @@ function truthCheck(collection, pre) {
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+ 
+function addTogether() {
+  return false;
+}
+
+addTogether(2,3);
