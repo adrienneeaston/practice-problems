7e3be25aab7f9b50a91502f662e236eefc5217b5
Adrienne <adrienneeaston@gmail.com>
2020-07-25 00:53:58 -0700

    broken

--- intermediate-algorithms.js
+++ intermediate-algorithms.js
@@ -591,3 +591,43 @@ function telephoneCheck(str) {
 }
 
 telephoneCheck("555-555-5555");
+
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+function checkCashRegister(price, cash, cid) {
+  let change;
+  let obj = {};
+  let val = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  cid = cid.reverse();
+
+  let diff = cash - price;
+  for(let i = val.length - 1; i > 0; i--) {
+    if(diff/val[i] > 0) {
+
+    }
+
+  }
+
+  function checkDrawer(total) {
+    let arr = [];
+    for(let i = 0; i < cid.length; i++) {
+      console.log("index", i);
+      if(total >= val[i]) {
+        let want = Math.floor(total/val[i]*val[i]);
+        let take = Math.min(want, cid[i][1]);
+        console.log("take", take);
+        total -= take;
+        arr.push([cid[i][0], take]);
+
+      }
+      // console.log("total", total);
+    }
+    return arr;
+  }
+
+  console.log(checkDrawer(25));
+
+  return change;
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
