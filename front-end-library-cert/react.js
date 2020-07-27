55f0cd76f7fbcea43325cb4d2694b6a859199167
Adrienne <adrienneeaston@gmail.com>
2020-07-27 15:41:42 -0700

    component with composition

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -12,3 +12,27 @@ class MyComponent extends React.Component {
     )
   }
 };
+
+// Create a component with composition 
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+          <ChildComponent />
+      </div>
+    );
+  }
+};
