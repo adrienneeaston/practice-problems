d56c955a89a2771b700efa0c1b046636a55a4300
Adrienne <adrienneeaston@gmail.com>
2020-07-27 15:28:34 -0700

    create react componant

---
+++ front-end-library-cert/react.js
@@ -0,0 +1,12 @@
+// create a react componant
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+  }
+};
