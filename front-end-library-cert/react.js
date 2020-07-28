bcb6dcdcb44391b9eea2502c531f5295830eef5c
Adrienne <adrienneeaston@gmail.com>
2020-07-27 17:46:24 -0700

    pass array as props

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -168,3 +168,26 @@ class Calendar extends React.Component {
     );
   }
 };
+
+// Pass an array as props
+
+const List = (props) => {
+  return <p>{props.tasks.join(", ")}</p>
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["walk dog", "workout"]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["eat", "sleep", "write code"]} />
+      </div>
+    );
+  }
+};
