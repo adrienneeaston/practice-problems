ec324fde4f5f0fa7067be03e2cd9da0c242f9714
Adrienne <adrienneeaston@gmail.com>
2020-07-27 17:24:27 -0700

    react component

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -126,3 +126,21 @@ class TypesOfFood extends React.Component {
 };
 
 ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+
+// Write React componant from scratch
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
