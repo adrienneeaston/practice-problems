267074a611edb0c988beac33736ef8f42c10c155
Adrienne <adrienneeaston@gmail.com>
2020-07-29 23:50:06 -0700

    this.setState

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -365,3 +365,27 @@ class MyComponent extends React.Component {
   }
 };
 
+// Set state with this.setState
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      name: "React Rocks!"
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
