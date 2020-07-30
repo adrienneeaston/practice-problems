14f4bf4dc8adf5868643e35a96a5c3c9054f3260
Adrienne <adrienneeaston@gmail.com>
2020-07-29 23:40:54 -0700

    render state in teh user interface

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -328,3 +328,21 @@ class StatefulComponent extends React.Component {
   }
 };
 
+// render state in teh user interface
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.name }</h1>
+      </div>
+    );
+  }
+};
+
