68f4a6a3dbed2e6697a4ded079babd7082f42473
Adrienne <adrienneeaston@gmail.com>
2020-08-07 17:41:17 -0700

    controlled form

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -512,15 +512,50 @@ class ControlledInput extends React.Component {
 
   handleChange(event) {
     this.setState({input: event.target.value});
-    console.log("state", this.state.input)
   };
-  
+
   render() {
     return (
       <div>
         <input type="text" value={this.state.input} onChange={this.handleChange} />
         <h4>Controlled Input:</h4>
         <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+
+// create a controlled form
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+     submit: this.state.input
+   });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.input} onChange={this.handleChange} />
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
       </div>
     );
   }
