d303254b06e4572424be4a27caeaef1f214e53ff
Adrienne <adrienneeaston@gmail.com>
2020-08-08 22:03:38 -0700

    array.filter to dynamically filter an array

--- front-end-library-cert/react.js
+++ front-end-library-cert/react.js
@@ -1119,4 +1119,56 @@ function Frameworks() {
   );
 };
 
+// use array.filter to dynamically filter an array
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    }
+  }
+  render() {
+    let keyId = 0;
+    const usersOnline = this.state.users.filter(user => user.online); 
+    const renderOnline = usersOnline.map(element => {
+      keyId++;
+      return <li key={keyId}>{element.username}</li>;
+    });
+    return (
+       <div>
+         <h1>Current Online Users:</h1>
+         <ul>
+           {renderOnline}
+         </ul>
+       </div>
+    ); 
+  }
+};
+
 
