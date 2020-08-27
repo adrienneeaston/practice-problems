51fc79ce7154319ff27dcf90f246df5bd53335bd
Adrienne <adrienneeaston@gmail.com>
2020-08-26 23:58:34 -0700

    handling multiple actions with switch

--- front-end-library-cert/redux.js
+++ front-end-library-cert/redux.js
@@ -64,3 +64,36 @@ const loginAction = () => {
     type: 'LOGIN'
   }
 };
+
+// Use a switch statement to handle multiple actions
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'LOGIN':
+      return {authenticated: true};
+      break;
+    case 'LOGOUT':
+      return {authenticated: false};
+      break;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
