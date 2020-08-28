03c18185dd596bb9b2438be21f82dc9aa5ebf437
Adrienne <adrienneeaston@gmail.com>
2020-08-28 16:15:32 -0700

    register a store listener

--- front-end-library-cert/redux.js
+++ front-end-library-cert/redux.js
@@ -141,3 +141,33 @@ const logoutUser = () => {
     type: LOGOUT
   }
 };
+
+// Register a store listener
+
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+let count = 0;
+
+function handleAdd() {
+  count++;
+}
+
+const add = store.subscribe(handleAdd);
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
