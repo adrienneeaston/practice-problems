64e5bf392f97d4afad8b8cb0e66ae9c82230208c
Adrienne <adrienneeaston@gmail.com>
2020-08-28 23:58:05 -0700

    spread operator with arrays redux

--- front-end-library-cert/redux.js
+++ front-end-library-cert/redux.js
@@ -350,3 +350,23 @@ const addToDo = (todo) => {
 }
 
 const store = Redux.createStore(immutableReducer);
+
+// Use the spread operator on arrays
+
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      return [...state, action.todo];
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
